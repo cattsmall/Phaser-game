58bb4dd921bcb5a9ca4ae485ac6b2bfa398ffde1
Catt Small <cattsmall@gmail.com>
2014-04-24 23:39:19 -0400

    Switching to Phaser

--- scripts/_Player.js
+++
@@ -1,6 +0,0 @@
-define([
-  'Player'
-], function(Player){
-
-
-});
--- scripts/game.js
+++ scripts/game.js
@@ -1,49 +1,16 @@
-require([
-  'frozen/GameCore',
-  './scripts/_Player.js',
-  'dojo/keys'
-], function(GameCore, keys, Player){
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-  //setup a GameCore instance
-  var game = new GameCore({
-    canvasId: 'canvas',
-    width:640,
-    height:480,
-    draw: function(context){
-      context.clearRect(0, 0, this.width, this.height);
-      context.fillRect(0, 0, this.width, this.height);
-      context.fillStyle = "#ff9900";
-      context.fillRect(player.x, player.y, player.size, player.size);
-    },
-    initInput: function(){
-      //tells the input manager to listen for key events
-      this.inputManager.addKeyAction(keys.LEFT_ARROW);
-      this.inputManager.addKeyAction(keys.RIGHT_ARROW);
-      this.inputManager.addKeyAction(keys.UP_ARROW);
-      this.inputManager.addKeyAction(keys.DOWN_ARROW);
-    },
-    update: function(millis){
+function preload() {
 
-      //just an example showing how to check for presses, could be done more effeciently
+  game.load.image('sky', 'assets/sky.png');
+  game.load.image('ground', 'assets/platform.png');
+  game.load.image('star', 'assets/star.png');
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+}
 
-      if(this.inputManager.keyActions[keys.LEFT_ARROW].isPressed() && player.x >= 0 ){
-        player.x-= player.speed;
-      }
+function create() {
+  game.add.sprite(0, 0, 'star');
+}
 
-      if(this.inputManager.keyActions[keys.RIGHT_ARROW].isPressed() && player.x <= (this.width - player.size) ){
-        player.x+= player.speed;
-      }
-
-      if(this.inputManager.keyActions[keys.UP_ARROW].isPressed() && player.y >= 0 ) {
-        player.y-= player.speed;
-      }
-
-      if(this.inputManager.keyActions[keys.DOWN_ARROW].isPressed() && player.y <= (this.height - player.size) ){
-        player.y+= player.speed;
-      }
-    }
-  });
-
-  //launch the game!
-  game.run();
-});
+function update() {
+}
