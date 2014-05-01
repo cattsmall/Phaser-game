7069dca057edad702f2c8b1174f478ddd24a584a
Catt Small <cattsmall@gmail.com>
2014-05-01 09:12:12 -0400

    Enemies added, games being set up for CLF class

--- scripts/game.js
+++ scripts/game.js
@@ -1,70 +1,72 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+  preload: preload,
+  create: create,
+  update: update
+});
 
 function preload() {
 
   game.load.image('sky', 'assets/sky.png');
   game.load.image('ground', 'assets/platform.png');
+  
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.spritesheet('baddie', 'assets/baddie.png', 128 , 32);
 }
+
+var player;
+
+var enemies;
+var livingEnemies = [];
+
+// HP
+var hitPoints;
+
+var score = 0;
+// IF we have time, we'll print the score
+var scoreText;
+var scoreString = '';
 
 function create() {
 
-  //  We're going to be using physics, so enable the Arcade Physics system
-  game.physics.startSystem(Phaser.Physics.ARCADE);
-
-  //  A simple background for our game
-  game.add.sprite(0, 0, 'sky');
-
-  //  The platforms group contains the ground and the 2 ledges we can jump on
-  platforms = game.add.group();
-
-  //  We will enable physics for any object that is created in this group
-  platforms.enableBody = true;
-
-  // Here we create the ground.
-  var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-  //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-  ground.scale.setTo(2, 2);
-
-  //  This stops it from falling away when you jump on it
-  ground.body.immovable = true;
-
-  //  Now let's create two ledges
-  var ledge = platforms.create(400, 400, 'ground');
-
-  ledge.body.immovable = true;
-
-  ledge = platforms.create(-150, 250, 'ground');
-
-  ledge.body.immovable = true;
-  
-  // The player and its settings
   player = game.add.sprite(32, game.world.height - 150, 'dude');
-
-  //  We need to enable physics on the player
-  game.physics.arcade.enable(player);
-
-  //  Player physics properties. Give the little guy a slight bounce.
-  player.body.bounce.y = 0.2;
-  player.body.gravity.y = 300;
-  player.body.collideWorldBounds = true;
-
-  //  Our two animations, walking left and right.
-  player.animations.add('left', [0, 1, 2, 3], 10, true);
-  player.animations.add('right', [5, 6, 7, 8], 10, true);
+  game.physics.enable(player, Phaser.Physics.ARCADE);
   
+  // Hitpoints
+  hitPoints = 5;
+  
+  // Keyboard input
   cursors = game.input.keyboard.createCursorKeys();
+  
+  //  The baddies!
+  enemies = game.add.group();
+  enemies.enableBody = true;
+  enemies.physicsBodyType = Phaser.Physics.ARCADE;
+
+  createEnemies();
   
 }
 
+function createEnemies () {
+
+    for (var y = 0; y < 4; y++)
+    {
+        for (var x = 0; x < 10; x++)
+        {
+            var enemy = enemies.create(x * 48, y * 50, 'baddie');
+            enemy.anchor.setTo(0.5, 0.5);
+            enemy.body.moves = false;
+        }
+    }
+
+    enemy.x = 100;
+    enemy.y = 50;
+}
+
 function update() {
-  //  Collide the player and the stars with the platforms
-  game.physics.arcade.collide(player, platforms);
-  
-  //  Reset the players velocity (movement)
-      player.body.velocity.x = 0;
+   
+      //  Reset the player, then check for movement keys
+          player.body.velocity.setTo(0, 0);
 
       if (cursors.left.isDown)
       {
@@ -73,6 +75,7 @@ function update() {
 
           player.animations.play('left');
       }
+      
       else if (cursors.right.isDown)
       {
           //  Move to the right
@@ -80,17 +83,21 @@ function update() {
 
           player.animations.play('right');
       }
-      else
+      
+      if (cursors.up.isDown)
       {
-          //  Stand still
-          player.animations.stop();
+          //  Move to the right
+          player.body.velocity.y = -150;
 
-          player.frame = 4;
+          player.animations.play('right');
       }
-    
-      //  Allow the player to jump if they are touching the ground.
-      if (cursors.up.isDown && player.body.touching.down)
+      
+      else if (cursors.down.isDown)
       {
-          player.body.velocity.y = -350;
+          //  Move to the right
+          player.body.velocity.y = 150;
+
+          player.animations.play('right');
       }
+
 }
